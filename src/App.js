9ab9b688dc2538a96abc74732d44e39971d994c1
Admin <patareharsh@gmail.com>
2024-03-06 19:57:34 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 
 import './App.scss';
-import {Routes,Route } from 'react-router-dom'
+import {Routes,Route, HashRouter } from 'react-router-dom'
 import Layout from  './components/Layout';
 import Home from './components/Home';
 function App() {
   return (
   <>
+  <HashRouter>
   <Routes>
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
       </Route>
   </Routes>
-
+  </HashRouter>
   </>
   );
 }
